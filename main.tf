218e991ca8df37f1d35870a2586fd9fe7a818977
ManojDangi1987 <100505890+ManojDangi1987@users.noreply.github.com>
2023-02-25 18:05:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,26 @@
-provider "google"{
+provider "google" {
+  project = "cvoproject"
+  credentials = "cvoproject.json"
+}
+
+resource "google_compute_network" "vpc_network" {
+name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+name = "terraform-instance"
+machine_type = "f1-micro"
+zone = "asia-east1-b"
+boot_disk {
+initialize_params {         
+image = "centos-cloud/centos-7"
+}
+}
+
+network_interface {
+network = google_compute_network.vpc_network.name
+access_config {
+}
+}
 
 }
 
